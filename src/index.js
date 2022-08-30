76a1bc2b92257f49e3b05ab2911f39b44747b52c
jrichardsz <jrichardsz.java@gmail.com>
2022-08-29 20:12:27 -0500

    expose internal modules to be used as library

---
+++ src/index.js
@@ -0,0 +1,1 @@
+exports.SpaServer = require("./server/serve.js");
--- test/serve.test.js
+++ test/serve.test.js
@@ -24,7 +24,7 @@ describe('Serve SPA', () => {
   })  
 
   it('gets the full settings and deploys html', async () => {
-    const createdServer = await startServer(app, allowedExt, 1, true, 'test/mocks/dist/test-setting-hack', 'test/mocks/settings.json');
+    const createdServer = await startServer(app, allowedExt, 0, true, 'test/mocks/dist/test-setting-hack', 'test/mocks/settings.json');
     server = createdServer.server;
 
     request(createdServer.app).get('/').expect("Content-type",/html/).expect(200).end(function(err, res) {
@@ -45,7 +45,7 @@ describe('Serve SPA', () => {
   })
 
   it('routes not allowed', async () => {
-    const createdServer = await startServer(app, allowedExt, 1, false, 'test/mocks/dist/test-setting-hack', 'test/mocks/settings.json');
+    const createdServer = await startServer(app, allowedExt, 0, false, 'test/mocks/dist/test-setting-hack', 'test/mocks/settings.json');
     server = createdServer.server;
 
     request(createdServer.app).get('/').expect("Content-type",/html/).expect(200).end(function(err, res) {
@@ -62,7 +62,7 @@ describe('Serve SPA', () => {
   })
 
   it('not settings file', async () => {
-    const createdServer = await startServer(app, allowedExt, 1, false, 'test/mocks/dist/test-setting-hack');
+    const createdServer = await startServer(app, allowedExt, 0, false, 'test/mocks/dist/test-setting-hack');
     server = createdServer.server;
 
     request(createdServer.app).get('/settings.json').expect("Content-type",/json/).expect(200).end(function(err, res) {
@@ -72,13 +72,13 @@ describe('Serve SPA', () => {
 
   it('error in promise', async () => {
     const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-    await startServer(app, allowedExt, 1, false, 'test/mocks/dist/test-setting-hack', 'test/mocks/things.json');
+    await startServer(app, allowedExt, 0, false, 'test/mocks/dist/test-setting-hack', 'test/mocks/things.json');
     expect(mockExit).toHaveBeenCalledTimes(1);
     expect(mockExit).toHaveBeenCalledWith(-1);
   })
 
   it('returns correct types', async () => {
-    const createdServer = await startServer(app, allowedExt, 1, false, 'test/mocks/dist/test-setting-hack');
+    const createdServer = await startServer(app, allowedExt, 0, false, 'test/mocks/dist/test-setting-hack');
     server = createdServer.server;
     const appReceived = createdServer.app;
     expect(appReceived).toBeTruthy();
